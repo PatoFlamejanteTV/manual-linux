4e2d4ab2a25d1739af43d8fd8f81d5338579a656
UltimateQuack <patoflamejantetv@gmail.com>
2025-12-10 13:30:25 -0300

    Update generate_sitemap.sh

--- generate_sitemap.sh
+++ generate_sitemap.sh
@@ -1,1 +1,12 @@
+# ferramenta simples que praticamente procura no site links d epágina sem links de outras páginas, etc.
+# usado na indexação do Google, Bing, etc.
+#
+# mais informações aqui: https://www.npmjs.com/package/sitemap-generator-cli
+#
+# se você quiser INSTALAR ele globalmente (eu acho), use "npm install -g sitemap-generator-cli"
+
 npx sitemap-generator-cli https://quack.is-a-good.dev/manual-linux --last-mod --change-freq daily --priority 1
+#                         [link do site]                           [incluir lastmod]
+#                                                                              [frequencia de mudanças]
+#                                                                                                [prioridade da
+#                                                                                                  indexação]

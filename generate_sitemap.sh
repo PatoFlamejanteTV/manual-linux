55b1d3cb029bdf1d40ec3179cfea8d5c43c0165b
UltimateQuack <patoflamejantetv@gmail.com>
2025-12-10 13:14:18 -0300

    corrijido um erro bem idiota do gerador de sitemaps

--- generate_sitemap.sh
+++ generate_sitemap.sh
@@ -1,1 +1,1 @@
-npx sitemap-generator-cli https://quack.is-a-good.dev --last-mod --change-freq daily --priority 1
+npx sitemap-generator-cli https://quack.is-a-good.dev/manual-linux --last-mod --change-freq daily --priority 1

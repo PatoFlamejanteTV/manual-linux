714210fa2fc8070e43427dc99e05de1564f71b55
UltimateQuack <patoflamejantetv@gmail.com>
2025-12-10 11:40:43 -0300

    Update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,1 +1,4 @@
+# Ainda estou testando o mdl, não sei ainda como ele funciona, ele só lista uns avisos
+# sobre a formataçao e não realmente muda nada, se alguém saber alguma ferramenta que
+# automaticamente formata corretamente, por favor me avise! - Pato
 mdl .

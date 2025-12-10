edf85458102554745afba6ff3a3ccef196effe8e
UltimateQuack <patoflamejantetv@gmail.com>
2025-12-10 19:24:52 -0300

    fixes

--- generate_sitemap.sh
+++ generate_sitemap.sh
@@ -4,8 +4,9 @@
 # mais informações aqui: https://www.npmjs.com/package/sitemap-generator-cli
 #
 # se você quiser INSTALAR ele globalmente (eu acho), use "npm install -g sitemap-generator-cli"
-
+cd site
 npx sitemap-generator-cli https://quack.is-a-good.dev/manual-linux --last-mod --change-freq daily --priority "1.0,0.95 0.90,0.85,0.80," --verbose
+cd ..
 #                         [link do site]                           [incluir lastmod]                                                  [incluir mais
 #                                                                              [frequencia de mudanças]                                 detalhes]
 #                                                                                                [prioridade da
